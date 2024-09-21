b37374e918bd09e9ebc170c82308027da02d56c1
alek <wait82985@gmail.com>
2024-09-21 22:49:21 +0500

    initial commit

---
+++ src/main/java/Solution.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> res = new ArrayList<>();
+
+        return res;
+    }
+}
